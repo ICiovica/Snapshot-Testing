82c414aab547d9b3e2a0e66689fa9a63a3a89618
Ionut Ciovica <ionut.ciovica@gamesys.co.uk>
2022-10-23 20:44:11 +0100

    Initial Commit

---
+++ Snapshot Testing/Snapshot_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snapshot_TestingApp.swift
+//  Snapshot Testing
+//
+//  Created by Ionut Ciovica on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Snapshot_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
